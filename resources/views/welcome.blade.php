5131cd5e0d6716aab4ef87a00aa4c3eaa18fe256
Steven Ulloa <ulloadeifheltsteven@gmail.com>
2022-10-13 17:04:42 -0600

    Subiendo Proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1, width=device-width">
+<title>MoaT</title>
+<meta name="description" content>
+<link rel="stylesheet" href="css/tipografia.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;900&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat Alternates:wght@400;500&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500;700&display=swap">
+<link rel="stylesheet" href="css/leanding.css">
+</head>
+
+<body>
+
+<header>
+
+  <div class="navbar navbar-default navbar-full">
+
+        <img src="assets\images\moat-logo.png" width="130" height="95" alt="homepage" class="contenedor-logo">
+
+        <nav>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Planes</a></li>
+            <li><a href="#">Complementos</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Iniciar Sesion</a></li>
+            <li><a href="#">Soporte</a></li>
+
+        </nav>
+        </div>
+</header>
+
+
+</body>
+
+
+
+
+
+
+</body>
+</html>
