a5a2893e97945c185453d41924c06a1378eba913
Steven Ulloa <ulloadeifheltsteven@gmail.com>
2022-10-17 13:36:29 -0600

    Co-authored-by: Jonathanc14 <Jonathanc14@users.noreply.github.com> Co-authored-by: ObedJp <ObedJp@users.noreply.github.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,107 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="initial-scale=1, width=device-width">
-<title>MoaT</title>
-<meta name="description" content>
-<link rel="stylesheet" href="css/tipografia.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;900&display=swap">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat Alternates:wght@400;500&display=swap">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500;700&display=swap">
-<link rel="stylesheet" href="css/leanding.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
+<title>Moat</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.min.css">
+<link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+<link rel="stylesheet" type="text/css" href="css/slick.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/custom.css">
 </head>
 
 <body>
-
-<header>
-
-  <div class="navbar navbar-default navbar-full">
-
-        <img src="assets\images\moat-logo.png" width="130" height="95" alt="homepage" class="contenedor-logo">
-
-        <nav>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Servicios</a></li>
-            <li><a href="#">Planes</a></li>
-            <li><a href="#">Complementos</a></li>
-            <li><a href="#">Nosotros</a></li>
-            <li><a href="#">Iniciar Sesion</a></li>
-            <li><a href="#">Soporte</a></li>
-
-        </nav>
+<div id="header-holder" class="main-header">
+    <div class="bg-animation">
+        <div class="graphic-show">
+            <img class="fix-size" src="images/graphic1.png" alt="">
+            <img class="img img2" src="images/graphic1.png" alt="">
+            <img class="img img2" src="images/graphic2.png" alt="">
+            <img class="img img2" src="images/graphic3.png" alt="">
         </div>
-</header>
+    </div>
 
+    <nav id="nav" class="navbar navbar-default navbar-full">
+        <div class="container-fluid">
+            <div class="container container-nav">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="navbar-header">
+                            <button aria-expanded="false" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
 
-</body>
+                            <a href="#">
+                            <img src="images\moat-logo.png" width="130" height="95" alt="homepage" class="contenedor-logo">
+                            </a>
 
+                        </div>
+                        <div style="height: 1px;" role="main" aria-expanded="false" class="navbar-collapse collapse" id="bs">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="index.html">Inicio</a></li>
+                                <li><a href="#">Servicios</a></li>
+                                <li><a href="#">Planes</a></li>
+                                <li><a href="#">Complementos</a></li>
+                                <li><a href="#">Nosotros</a></li>
 
+                                <li class="support-button-holder support-dropdown">
+                                    <a class="support-button" href="#">Iniciar Sesion</a>
+                                </li>
 
+                                <li class="support-button-holder support-dropdown">
+                                    <a class="support-button" href="#">Soporte</a>
+                                </li>
 
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 
-
+    <div id="top-content" class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div id="main-slider">
+                        <div class="slide">
+                            <div class="big-title">
+                                <h1>Dedicado a Instituciones Educativas.</h1>
+                            </div>
+                            <div class="spacer"></div>
+                            <div class="big-title">Todo en <span>un solo lugar,</span><br>donde sea, cuando sea.</div>
+                            <p>Moat es el primer paso para el desarrollo de sistemas autosuficientes dedicados a las instituciones educativas, empresas y el usuario comun.</p>
+                            <div class="btn-holder">
+                                <a href="#" class="ybtn ybtn-header-color">Registrarse</a><a href="#" class="ybtn ybtn-white ybtn-shadow">Acerca de...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img class="header-graphic" src="images/graphic1.png" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="info" class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="info-text">Moat es un proyecto creado por el equipo de desarrollo llamado Syntax, nacio de la necesidad de implementar un sistema multitarea que posea todas las caracteristicas de gestion, administrativas y de comunicacion.</div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-slider.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
